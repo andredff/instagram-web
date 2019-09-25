5d12e55267100ae5c5db90713337412a31a6de11
Andre Souza <andre.souza@solucioneonline.com.br>
2019-09-25 20:20:09 -0300

    animations & model Imagem

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in')),
+      // transition('visivel => escondido', animate('1s ease-in'))
+
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'escondido';
 
   constructor() { }
 
   ngOnInit() {
+
+    setInterval(()=> {
+      this.estado = 'visivel'
+    }, 1000);
+    setInterval(()=> {
+      this.estado = 'escondido'
+    }, 6000);
+
   }
 
 }
