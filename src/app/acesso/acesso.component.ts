b84496273b0c8251f2fa40010d9d040deb5d1992
Andre Souza <andre.souza@solucioneonline.com.br>
2019-09-25 21:23:46 -0300

    creating animations for login/signup

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.scss']
+  styleUrls: ['./acesso.component.scss'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-60px, 0)' }),
+        animate('600ms 0s ease-in-out') // ducarao, delay e aceleracao
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(60px, 0)' }),
+        animate('600ms 0s ease-in-out') // ducarao, delay e aceleracao
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
+
+  public estadoBanner = 'criado';
+  public estadoPainel = 'criado';
 
   constructor() { }
 
